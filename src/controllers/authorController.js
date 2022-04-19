a0c57bb6594d24827d837b074eb213083948acee
sankalesh8668229890 <sankalesh.harak1998@gmail.com>
2022-04-19 18:22:00 +0530

    done 18april

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -3,6 +3,7 @@ const authorModel= require("../models/authorModel")
 const createAuthor= async function (req, res) {
     let author = req.body
     let authorCreated = await authorModel.create(author)
+    console.log(author);
     res.send({data: authorCreated})
 }
 
